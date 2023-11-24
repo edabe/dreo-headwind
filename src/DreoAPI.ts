5a4a0bfbb1a90323de41bb263616d0b9369c375b
edabe <eabe.mobile@gmail.com>
2023-11-24 18:45:27 -0500

    fix: Increase timeout in oscillation; Make log move verbose for testing

--- src/DreoAPI.ts
+++ src/DreoAPI.ts
@@ -161,11 +161,11 @@ export class DreoAPI {
     await this.authenticate();
     // open websocket
     const url = `wss://wsb-${this.config.server}.dreo-cloud.com/websocket?accessToken=${this.auth?.access_token}&timestamp=${Date.now()}`;
-    this.config.logger.debug(url);
+    this.config.logger.debug('Start web socket url:', url);
     this.webSocket = new ReconnectingWebSocket(url, [], { WebSocket: WebSocket });
   
     this.webSocket.addEventListener('error', error => {
-      this.config.logger.debug('WebSocket', error);
+      this.config.logger.debug('WebSocket Error', error);
     });
   
     this.webSocket.addEventListener('open', () => {
@@ -188,12 +188,13 @@ export class DreoAPI {
       'params': parameters,
       'timestamp': Date.now()
     });
-    // this.config.logger.info('---- sending message', message);
+    this.config.logger.debug('DreoAPI sendCommand', JSON.stringify(message));
     this.webSocket?.send(message);
     await sleep(wait); // wait for the operation to complete
   }
 
   public disconnect(): void {
+    this.config.logger.debug('DreoAPI disconnect');
     clearInterval(this.webSocketTimer);
     this.webSocket?.close();
     this.webSocket = undefined;
@@ -202,6 +203,7 @@ export class DreoAPI {
 
   // Return device list
   public async getDevices(): Promise<Array<DreoDevice>> {
+    this.config.logger.debug('DreoAPI getDevices');
     await this.authenticate();
     let devices: Array<DreoDevice> = [];
     await axios.get(`https://app-api-${this.config.server}.dreo-cloud.com/api/v2/user-device/device/list`, {
@@ -237,12 +239,12 @@ export class DreoAPI {
           }))(device);
         });
       } else {
-        this.config.logger.error('error retrieving device list:', payload);
+        this.config.logger.error('Error retrieving device list:', payload);
         throw new Error('Unable to retrieve device list - API returned 200');
       }
     })
     .catch((error) => {
-      this.config.logger.error('error retrieving device list:', error);
+      this.config.logger.error('Error retrieving device list:', error);
       throw new Error('Unable to retrieve device list');
     });
     return devices;
@@ -250,6 +252,7 @@ export class DreoAPI {
 
   // used to initialize power state, speed values on boot
   public async getState(serialNumber: string): Promise<DreoState|null> {
+    this.config.logger.debug('DreoAPI getState', serialNumber);
     await this.authenticate();
     let state = null;
     await axios.get(`https://app-api-${this.config.server}.dreo-cloud.com/api/user-device/device/state`, {
@@ -287,30 +290,33 @@ export class DreoAPI {
             windlevel: mixed.windlevel.state
            }))(payload.data);
       } else {
-        this.config.logger.error('error retrieving device state:', payload);
+        this.config.logger.error('Error retrieving device state:', payload);
         throw new Error('Unable to retrieve device state - API returned 200');
       }
     })
     .catch((error) => {
-      this.config.logger.error('error retrieving device state:', error);
+      this.config.logger.error('Error retrieving device state:', error);
       throw new Error('Unable to retrieve device state');
     });
     return state;
   }
 
   public async getTemperature(deviceSn: string): Promise<number|undefined> {
+    this.config.logger.debug('DreoAPI getTemperature', deviceSn);
     const state = await this.getState(deviceSn);
     return state?.temperature as number;
   }
 
   public async airCirculatorPowerOn(deviceSn: string, powerOn: boolean): Promise<void> {
+    this.config.logger.debug('DreoAPI airCirculatorPowerOn', deviceSn, powerOn);
     const state = await this.getState(deviceSn);
-    if (!state?.poweron) {
+    if (state?.poweron !== powerOn) {
       await this.sendCommand(deviceSn, {'poweron': powerOn}, 0);
     }
   }
 
   public async airCirculatorOscillate(deviceSn: string, oscillation: AirCirculatorOscillation): Promise<void> {
+    this.config.logger.debug('DreoAPI airCirculatorOscillate', deviceSn, oscillation);
     await this.sendCommand(deviceSn, {'oscmode': oscillation}, 3000);
   }
 
@@ -325,10 +331,11 @@ export class DreoAPI {
    *                 Note: Horizontal range is [-60..60] and vertical range is [0..90]
    */
   public async airCirculatorPosition(deviceSn: string, position: [number, number]): Promise<void> {
+    this.config.logger.debug('DreoAPI airCirculatorPosition', deviceSn, position);
     const hAngle = position[0] < -60 ? -60 : position[0] > 60 ? 60 : position[0];
     const vAngle = position[1] < 0 ? 0 : position[1] > 90 ? 90 : position[1];
     await this.airCirculatorOscillate(deviceSn, AirCirculatorOscillation.NONE); // stop oscillation first
-    const delay = await this.getTimerMove(deviceSn, hAngle, vAngle);
+    const delay = 15000; // Need to wait about 15s before a 'fixedconf' command follows a change in oscillation 
     await this.sendCommand(deviceSn, {'fixedconf': `${vAngle.toString()},${hAngle.toString()}`}, delay); // positioning the fan might take a while
   }
 
@@ -336,7 +343,6 @@ export class DreoAPI {
    * Different than the Dreo API, this function takes a tuple for horizontal and a tuple for vertical.
    * 
    * @param deviceSn    Device serial number
-   * @param oscillation Oscillation mode: HORIZONTAL | VERTICAL | HORIZONTAL_VERTICAL
    * @param horizontal  Horizontal tuple where [0] is the direction and [1] is the oscillating angle
    *                    Examples: 
    *                    [0, 60]  : Fan will oscillate between horizontal angles -30 and 30
@@ -350,34 +356,35 @@ export class DreoAPI {
    *                    [30, 30]: Fan will oscillate between vertical angles 15 and 45
    *                    Note: Oscillating angles cannot exceed 0 and 90, and the minimum value for vertical[1] is 30
    */
-  public async airCirculatorCruise(deviceSn: string, oscillation: AirCirculatorOscillation, horizontal: [number, number], vertical: [number, number]): Promise<void> {
-    if (oscillation !== AirCirculatorOscillation.NONE) {
-      const hDirection = horizontal[0];
-      const hAngle = horizontal[1] < 30 ? 30 : horizontal[1] > 120 ? 120 : horizontal[1];
-      const hMin = Math.max(hDirection - Math.floor(hAngle / 2), -60);
-      const hMax = Math.min(hMin + hAngle, 60);
+  public async airCirculatorCruise(deviceSn: string, horizontal: [number, number], vertical: [number, number]): Promise<void> {
+    this.config.logger.debug('DreoAPI airCirculatorCruise', deviceSn, horizontal, vertical);
+    const hDirection = horizontal[0];
+    const hAngle = horizontal[1] < 30 ? 30 : horizontal[1] > 120 ? 120 : horizontal[1];
+    const hMin = Math.max(hDirection - Math.floor(hAngle / 2), -60);
+    const hMax = Math.min(hMin + hAngle, 60);
 
-      const vDirection = vertical[0];
-      const vAngle = vertical[1];
-      const vMin = Math.max(vDirection - Math.floor(vAngle / 2), 0);
-      const vMax = Math.min(vMin + vAngle, 90);
+    const vDirection = vertical[0];
+    const vAngle = vertical[1];
+    const vMin = Math.max(vDirection - Math.floor(vAngle / 2), 0);
+    const vMax = Math.min(vMin + vAngle, 90);
 
-      await this.airCirculatorOscillate(deviceSn, oscillation); // start oscillation first
-      const cruiseconf = `${vMax.toString()},${hMax.toString()},${vMin.toString()},${hMin.toString()}`;
-      await this.sendCommand(deviceSn, {'cruiseconf': cruiseconf}, 1000);
-    }
+    const cruiseconf = `${vMax.toString()},${hMax.toString()},${vMin.toString()},${hMin.toString()}`;
+    await this.sendCommand(deviceSn, {'cruiseconf': cruiseconf}, 1000);
   }
 
   public async airCirculatorSpeed(deviceSn: string, speed: number): Promise<void> {
+    this.config.logger.debug('DreoAPI airCirculatorSpeed', deviceSn, speed);
     const windLevel = speed < 1 ? 1 : speed > 9 ? 9 : speed;
     await this.sendCommand(deviceSn, {'windlevel': windLevel}, 0);
   }
 
   public async airCirculatorMode(deviceSn: string, mode: AirCirculatorMode): Promise<void> {
+    this.config.logger.debug('DreoAPI airCirculatorMode', deviceSn, mode);
     await this.sendCommand(deviceSn, {'mode': mode}, 1000);
   }
 
   public async airCirculatorCalibrate(deviceSn: string, orientation: AirCirculatorCalibration): Promise<void> {
+    this.config.logger.debug('DreoAPI airCirculatorCalibrate', deviceSn, orientation);
     await this.airCirculatorOscillate(deviceSn, AirCirculatorOscillation.NONE); // stop oscillation first
     if (orientation === AirCirculatorCalibration.HORIZONTAL_VERTICAL) {
       await this.airCirculatorCalibrate(deviceSn, AirCirculatorCalibration.HORIZONTAL);
