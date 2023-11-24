5553ac9f9d9c493cc38a0ec8ca1f94ae3b23b433
edabe <eabe.mobile@gmail.com>
2023-11-24 18:46:16 -0500

    fix: I've got promises wrong - fixing async calls

--- src/DreoProfile.ts
+++ src/DreoProfile.ts
@@ -9,11 +9,12 @@ import { AirCirculatorOscillation, DreoAPI } from "./DreoAPI";
 // Horizontal -15 to 15
 // Vertical 15 to 65
 const CRUISE_HORIZONTAL: [number, number] = [0, 30];
-const CRUISE_VERTICAL: [number, number] = [45, 30];
+const CRUISE_VERTICAL: [number, number] = [30, 30];
 
 export abstract class DreoProfile {
     name: string;
-    abstract apply(dreoSerialNumber: string, dreoApi: DreoAPI): void;
+    // All implementations must be async
+    abstract apply(dreoSerialNumber: string, dreoApi: DreoAPI): Promise<void>;
     toString(): string {
         return this?.name;
     }
@@ -21,65 +22,55 @@ export abstract class DreoProfile {
 
 class OscillateHorizontalProfile extends DreoProfile {
     name = 'OSCILATE_HORIZONTAL';
-    apply(serialNumber: string, dreoApi: DreoAPI) {
-        (async () => {
-            await dreoApi.airCirculatorPowerOn(serialNumber, true);
-            await dreoApi.airCirculatorPosition(serialNumber, [0, 45]);
-            await dreoApi.airCirculatorOscillate(serialNumber, AirCirculatorOscillation.HORIZONTAL);
-            await dreoApi.airCirculatorCruise(serialNumber, AirCirculatorOscillation.HORIZONTAL, CRUISE_HORIZONTAL, CRUISE_VERTICAL);
-        })();
+    async apply(serialNumber: string, dreoApi: DreoAPI) {
+        await dreoApi.airCirculatorPowerOn(serialNumber, true);
+        await dreoApi.airCirculatorPosition(serialNumber, [0, 30]);
+        await dreoApi.airCirculatorCruise(serialNumber, CRUISE_HORIZONTAL, CRUISE_VERTICAL);
+        await dreoApi.airCirculatorOscillate(serialNumber, AirCirculatorOscillation.HORIZONTAL);
     }
 }
 
 class OscillateVerticalProfile extends DreoProfile {
     name = 'OSCILATE_VERTICAL';
-    apply(serialNumber: string, dreoApi: DreoAPI) {
-        (async () => {
-            await dreoApi.airCirculatorPowerOn(serialNumber, true);
-            await dreoApi.airCirculatorPosition(serialNumber, [0, 45]);
-            await dreoApi.airCirculatorOscillate(serialNumber, AirCirculatorOscillation.HORIZONTAL);
-            await dreoApi.airCirculatorCruise(serialNumber, AirCirculatorOscillation.VERTICAL, CRUISE_HORIZONTAL, CRUISE_VERTICAL);
-        })();
+    async apply(serialNumber: string, dreoApi: DreoAPI) {
+        await dreoApi.airCirculatorPowerOn(serialNumber, true);
+        await dreoApi.airCirculatorPosition(serialNumber, [0, 30]);
+        await dreoApi.airCirculatorCruise(serialNumber, CRUISE_HORIZONTAL, CRUISE_VERTICAL);
+        await dreoApi.airCirculatorOscillate(serialNumber, AirCirculatorOscillation.VERTICAL);
     }
 }
 
 class OscillateHorizontalVerticalProfile extends DreoProfile {
     name = 'OSCILATE_HORIZONTAL_VERTICAL';
-    apply(serialNumber: string, dreoApi: DreoAPI) {
-        (async () => {
-            await dreoApi.airCirculatorPowerOn(serialNumber, true);
-            await dreoApi.airCirculatorOscillate(serialNumber, AirCirculatorOscillation.HORIZONTAL);
-            await dreoApi.airCirculatorCruise(serialNumber, AirCirculatorOscillation.HORIZONTAL_VERTICAL, CRUISE_HORIZONTAL, CRUISE_VERTICAL);
-        })();
+    async apply(serialNumber: string, dreoApi: DreoAPI) {
+        await dreoApi.airCirculatorPowerOn(serialNumber, true);
+        await dreoApi.airCirculatorCruise(serialNumber, CRUISE_HORIZONTAL, CRUISE_VERTICAL);
+        await dreoApi.airCirculatorOscillate(serialNumber, AirCirculatorOscillation.HORIZONTAL_VERTICAL);
     }
 }
 
-class Center45Degrees extends DreoProfile {
-    name = 'CENTER_45_DEGREE';
-    apply(serialNumber: string, dreoApi: DreoAPI) {
-        (async () => {
-            await dreoApi.airCirculatorPowerOn(serialNumber, true);
-            await dreoApi.airCirculatorPosition(serialNumber, [0, 45]);
-        })();
+class Center30Degrees extends DreoProfile {
+    name = 'CENTER_30_DEGREE';
+    async apply(serialNumber: string, dreoApi: DreoAPI) {
+        await dreoApi.airCirculatorPowerOn(serialNumber, true);
+        await dreoApi.airCirculatorPosition(serialNumber, [0, 30]);
     }
 }
 
 class Center0Degrees extends DreoProfile {
     name = 'CENTER_0_DEGREE';
-    apply(serialNumber: string, dreoApi: DreoAPI) {
-        (async () => {
-            await dreoApi.airCirculatorPowerOn(serialNumber, true);
-            await dreoApi.airCirculatorPosition(serialNumber, [0, 0]);
-        })();
+    async apply(serialNumber: string, dreoApi: DreoAPI) {
+        await dreoApi.airCirculatorPowerOn(serialNumber, true);
+        await dreoApi.airCirculatorPosition(serialNumber, [0, 0]);
     }
 }
 
-export enum DreoProfileType { 'CENTER_0', 'CENTER_45', 'HORIZONTAL', 'VERTICAL', 'HORIZONTAL_VERTICAL' }
+export enum DreoProfileType { 'CENTER_0', 'CENTER_30', 'HORIZONTAL', 'VERTICAL', 'HORIZONTAL_VERTICAL' }
 
 export const DreoProfiles = {
     [DreoProfileType.HORIZONTAL]: new OscillateHorizontalProfile(),
     [DreoProfileType.VERTICAL]: new OscillateVerticalProfile(),
     [DreoProfileType.HORIZONTAL_VERTICAL]: new OscillateHorizontalVerticalProfile(),
-    [DreoProfileType.CENTER_45]: new Center45Degrees(),
+    [DreoProfileType.CENTER_30]: new Center30Degrees(),
     [DreoProfileType.CENTER_0]: new Center0Degrees()
 }
